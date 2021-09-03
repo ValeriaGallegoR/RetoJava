02eefb92fd258179f93182a4b0725f2683b01117
ValeriaGallegoR <valeriagallego20@gmail.com>
2021-09-03 16:30:42 -0500

    Se subió el proyecto con la funcionalidad de número mayor e iguales declarando las variables

---
+++ src/main/java/controlador/Main.java
@@ -0,0 +1,10 @@
+package controlador;
+
+import modelo.EsMayor;
+
+public class Main {
+    public static void main(String[] args) {
+        EsMayor m = new EsMayor();
+        m.numMayor();
+    }
+}
